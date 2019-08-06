6be35698d8298e1a47247f8947b67c7daf89c78a
li.xiangyang <sapolang@sapolangdeMacBook-Pro.local>
2019-08-06 17:30:58 +0800

    ..

--- lnmp/php56/extensions/oci8/install-oci8.sh
+++ lnmp/php56/extensions/oci8/install-oci8.sh
@@ -1,12 +1,7 @@
 apt-get install -y libaio-dev
 cd /opt
-tar -zxvf /opt/instantclient_18_5.tar.gz
-echo "/opt/instantclient_18_5" >> /etc/ld.so.conf.d/oracle-instantclient.conf
-ldconfig
-tar -zxvf /opt/oci8-2.0.10.tgz
-cd /opt/oci8-2.0.10
-phpize
-./configure --with-oci8="instantclient,/opt/instantclient_18_5"
-make
-make install
-docker-php-ext-enable oci8
+tar -zxvf /opt/instantclient.tar.gz
+echo "/opt/instantclient" >> /etc/ld.so.conf.d/oracle-instantclient.conf && ldconfig
+
+docker-php-ext-configure oci8 --with-oci8="instantclient,/opt/instantclient" && docker-php-ext-install oci8
+docker-php-ext-configure pdo_oci --with-pdo-oci="instantclient,/opt/instantclient,12.1" && docker-php-ext-install pdo_oci
